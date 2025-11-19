a948e874b0908c7b753dc86b9a1c601ed883001b
Samyog pangeni <samyogpangeni4@gmail.com>
2025-11-19 19:26:18 +0545

    Adding my files to

---
+++ Basics/variables.js
@@ -0,0 +1,13 @@
+const username="Samyog"
+const birthyear=2003
+let name="Samyog"
+var password="@smgbeast"
+console.table([username,birthyear,name, password])
+
+// Datatypes in JS
+// int 
+// BigInt
+// null
+// undefined
+// String
+
