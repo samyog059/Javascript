bb40d71e590b4dac900356fb28ce6d5ce78ac588
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-20 19:43:12 +0545

    New updation

---
+++ Basics/typeconversion.js
@@ -0,0 +1,22 @@
+let score =null
+console.log(typeof score);
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(score);
+
+// "33"==33
+// "33abc"==NaN
+// true==1; false==0
+let iL =0
+console.log(iL);
+let Ok =Boolean(iL)
+console.log(Ok);
+// Ended this type conversion
+
+let num =33
+let strnum =String(num)
+console.log( typeof strnum);
+
+
+
+
