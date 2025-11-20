66aa16a7325d601733000627e3f40ac209738bfb
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-20 20:06:28 +0545

    added

---
+++ Basics/operations.js
@@ -0,0 +1,27 @@
+let value = 25
+let neg =-value
+console.log(neg)
+let a=25
+let b=5
+// performing all mathematical operations
+let sum=a+b
+let div =a/b
+let mod =a%b
+console.table([sum,div,mod])
+
+console.log("33"+2) //332
+console.log(33+"2");//332
+console.log(33+33+"2");//662 Javascript uses it common senses
+console.log(+true); // output is  1 instead of true because it is incremented
+console.log(+false); // .....Hope you understand
+let gamectr =100
+a++; // post fix 
+++a // prefix
+console.log(a);
+console.log(a);
+
+
+
+
+
+
