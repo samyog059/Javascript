35312afafb84e49c402f3d9eabf05b5daf3ffa4a
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-22 14:08:20 +0545

    new file added

---
+++ Basics/comparison.js
@@ -0,0 +1,22 @@
+console.log(2==1);
+console.log(2>1);
+console.log(null >=0);
+console.log("2"==2);
+console.log("02"==2);
+console.log(null>0);
+console.log(null==0);
+console.log(undefined>0);
+console.log("2"===2); // false triple = strictly checks datatypes also 
+console.log(2===2); // true because of same data type
+console.log(undefined==null);
+console.log(undefined<null);
+
+
+
+
+
+
+
+
+
+
