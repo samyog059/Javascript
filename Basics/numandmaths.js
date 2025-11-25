d41ce07968d4af6a4744ec7ca9f8eda9b23fbedb
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-25 20:18:11 +0545

    commited

---
+++ Basics/numandmaths.js
@@ -0,0 +1,18 @@
+const cash = 400
+const balance = new Number(500.44)
+console.log(balance);
+console.log(cash);
+const name = new String("Samyog")
+console.log(name);
+console.log(balance.toString().length);
+console.log(balance.toFixed()); // gives fixed values for decimal number 
+const otherNumber = 23.6966
+console.log(otherNumber.toPrecision(2)); // gives number according to the digits in precision parameters
+const millions = 100000000
+console.log(millions.toLocaleString('en-Np')) // this parameter converts nepali locale style in numbers with commas we just have to pass 'en-Np' in parameters
+const Lumba = 2360000
+console.log(Lumba);
+
+
+
+
