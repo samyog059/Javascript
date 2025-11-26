5a49483cbbb391ae3042c2793634696d11087fce
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-26 20:52:09 +0545

    Commited array

---
+++ Basics/Intermediate/array.js
@@ -0,0 +1,16 @@
+const Myfriends = ["RAm","Shyam","Suman"]
+console.log(Myfriends[1]); // calling array with index starts from
+const MyEnemies = new Array("Alu","Kalu","Bhalu")
+console.log(MyEnemies[0]);
+Myfriends.push("Rohan") // add value in last index
+console.log(Myfriends);
+MyEnemies.pop() // deletes the  last value
+
+Myfriends.shift() // deletes the first index value
+
+Myfriends.unshift("Gulshan") // add value in first index
+MyEnemies[2]="Bom" // replaces values to given index
+console.log(MyEnemies[2]);
+console.log(Myfriends);
+
+
