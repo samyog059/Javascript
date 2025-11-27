83c4019f5e6d143104fac00d692d02ea7a6dd2de
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-27 19:29:13 +0545

    Success

---
+++ Intermediate/practicearray.js
@@ -0,0 +1,7 @@
+const TeamLYT = ["Samyog","Sharada","Manoj","Yagnya","Pratibha","Sonica"]
+const Numofmem = TeamLYT.length
+TeamLYT.sort()
+for(i=0;i<TeamLYT.length; i++){
+    console.log(TeamLYT[i]);
+    
+}
