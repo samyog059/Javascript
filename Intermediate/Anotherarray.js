fabf15ed01785a17eff08c54b43d8a459264d1b3
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-27 19:16:42 +0545

    Pushed

---
+++ Intermediate/Anotherarray.js
@@ -0,0 +1,14 @@
+const Fruits = ["Kiwi","Pineapple","Apple","Banana"]
+console.log(Fruits[3]); // Output: "Banana"
+
+let Totalfruits = Fruits.length // No parentheses needed
+// console.log(Totalfruits); // Output: 4
+
+// let Index = Fruits.indexOf("Apple")
+// console.log(Index); // finds the index of given array
+
+Fruits.sort() // Sorts the array alphabetically in place
+
+for(i=0;i<Fruits.length;i++){
+    console.log(Fruits[i]);
+}
