832dd40e312238fb43267aa25e1051de02872eb4
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-27 20:48:50 +0545

    cm

---
+++ Intermediate/object.js
@@ -0,0 +1,11 @@
+// singleton
+// object literals
+
+const User = {
+    "Full name": "Samyog",  // Property with spaces must be in quotes
+    College: "Waling Multiple Campus",
+    Class: 15,
+    LastLogindays: ["samyog", "Pangeni"]
+}
+
+console.log(User["Full name"]); // Use bracket notation without dot
