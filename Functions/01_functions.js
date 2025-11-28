4209e7d09971aa827cdb91d6d5c56a987d9fcb5d
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-28 20:01:16 +0545

    execute

---
+++ Functions/01_functions.js
@@ -0,0 +1,10 @@
+// A simple function declaration named "Myname"
+function Myname() {
+    // This line prints your name on the console
+    console.log("Samyog Pangeni");
+}
+
+// Calling (running) the function three times
+Myname();   // 1st call
+Myname();   // 2nd call
+Myname();   // 3rd call
---
+++ Functions/02_functions.js
@@ -0,0 +1,9 @@
+// A function named "AddNum" that takes two numbers as input
+function AddNum(num1, num2) {
+    return num1+num2
+}
+
+// Calling the function and passing 3 and 9 as arguments
+const result = AddNum(3, 9);   // Output: 12
+console.log("The sum of two number is:"+result);
+
