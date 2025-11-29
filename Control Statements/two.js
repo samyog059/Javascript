dd386fd7ded3ee3fcf0e7bff29e5d5cdca14c7f9
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-29 13:36:08 +0545

    added

---
+++ Control Statements/two.js
@@ -0,0 +1,18 @@
+const temperature =25
+if(temperature>40){
+    console.log("The weather is too hot");
+    
+}
+else if(temperature>35) {
+    console.log("The temperature is hot");
+    
+}
+else{
+    const samyog= "Manxe"
+    console.log(`Hey ${samyog} the weather is good`);
+    
+}
+
+const Money =1000
+if(Money>500) console.log("Test")
+    
