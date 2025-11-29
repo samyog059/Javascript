268a96f5c20ead5ceef4632e16e55b21a596832f
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-11-29 20:46:27 +0545

    HEy

---
+++ Control Statements/three.js
@@ -0,0 +1,15 @@
+const a=25
+const b=75
+const c=65
+if(a>b && a>c){
+    console.log(`Largest number is ${a}`);
+    
+}
+else if(b>c && b>a){
+    console.log(`Largest number is ${b}`);
+    
+}
+else{
+    console.log(`Largest number is ${c}`);
+    
+}
