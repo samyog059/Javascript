fb08e88d3601cf1d662132506e3d072baa24cc31
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-12-01 19:09:37 +0545

    changes

---
+++ Control Statements/four.js
@@ -0,0 +1,19 @@
+// Switch : It is a efficient replacement of many else if statements 
+// Lets understand the procedure
+let Month = 25;
+switch(Month){
+    case 1:
+        console.log("It is january");
+        break
+    case 12:
+        console.log("It is december");
+        break
+    case 11:
+        console.log("It is no nut november");
+        break
+    default:
+        console.log("Please Enter valid month");
+        
+        
+        
+}
