cbb83c74c45a9ad3740d8ffc15a109bceba4ee24
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-12-01 19:16:45 +0545

    something to push

---
+++ Control Statements/five.js
@@ -0,0 +1,26 @@
+let marks = 95;
+
+switch (true) {
+    case marks >= 90:
+        console.log("A+");
+        break;
+
+    case marks >= 80:
+        console.log("A");
+        break;
+
+    case marks >= 70:
+        console.log("B+");
+        break;
+
+    case marks >= 60:
+        console.log("B");
+        break;
+
+    case marks >= 50:
+        console.log("C");
+        break;
+
+    default:
+        console.log("Fail");
+}
