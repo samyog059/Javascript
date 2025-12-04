4cff861a2354905c5bb083b12cf73569f71c71a5
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-12-04 20:33:10 +0545

    push fush

---
+++ Control Statements/thirteen.js
@@ -0,0 +1,8 @@
+let i=1
+while (i<=5) {
+    i++ // this increments variable value before printing.. So this will automatically incremented the variable value
+    console.log(i);
+    i++
+    
+    
+}
