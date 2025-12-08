88cc4c641c0422eac60ab93618f1fdaab058e23f
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-12-08 20:50:14 +0545

    new addons

---
+++ DOM Javascript/Element _selector/script.js
@@ -0,0 +1,10 @@
+//elements selectors
+// 1.document.getElementById()
+// 2.document.getElementsByTagName()
+// 3.document.getElementsByClassName()
+// 4.document.querySelector()
+// 5.document.querySelectorAll()
+
+const docs = document.getElementById("hell")
+docs.style.color="Red"
+docs.style.backgroundColor="black"
