0e2d37b37ef692ff520498c2e883a169104faca5
Samyog Pangeni <samyogpangeni4@gmail.com>
2025-12-10 07:34:20 +0545

    new

---
+++ DOM Javascript/Modify/script.js
@@ -0,0 +1,7 @@
+<script>
+  let div = document.createElement('div');
+  div.className = "alert";
+  div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
+
+  document.body.append(div);
+</script>
